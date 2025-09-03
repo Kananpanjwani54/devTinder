c5a901357e83d3d53c42b1858d4f2c1f62a8be21
Kananpanjwani54 <kananpanjwani68@gmail.com>
2025-09-03 17:37:50 +0530

    testing commands http

--- src/app.js
+++ src/app.js
@@ -1,32 +1,26 @@
-const express=require("express");
-//created app for express-->basically an object inside a var app;
-// entry point fot http
+const express = require("express");
 
-const app=express();
+// create app
+const app = express();
 
-//route handler
 
-// app.use("/",(req,res)=>{
-// res.send("Hello from Server of kanan!!")
-// });
-app.use("/test",(req,res)=>{
-res.send("Main jian!")
+// GET route
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Kanan", lastname: "Panjwani" });
 });
-// app.use("/hello",(req,res)=>{
-// res.send("Main Honn don!")
-// });
-// app.use("/hello/123",(req,res)=>{
-// res.send("Main Honn don 2!")
-// });
-app.get("/hello", (req, res) => res.send("Just hello"));
-app.get("/hello/:id", (req, res) => res.send("Hello with id"));
 
-const PORT=3000;
+// POST route (save user)
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  res.send("Data successfully saved to database!");
+});
 
-//
-app.listen(PORT,()=>{
-    console.log("Server running on http://localhost:3000")
-})
-//nodemon install to not restarting server manually.(-g-->global)
+// DELETE route (delete by id)
+app.delete("/user", async (req, res) => {
+  res.send(`Data successfully deleted`);
+});
 
-
+// start server
+app.listen(3000, () => {
+  console.log("Server is listening");
+});
