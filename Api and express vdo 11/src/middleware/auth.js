a4f3447c39e205bed225e5dd5ce1f664f843f8af
kananpanjwani682gmail.com <kananpanjwani68@gmail.com>
2025-09-11 13:18:13 +0530

    auth created

--- Api and express vdo 11/src/middleware/auth.js
+++ Api and express vdo 11/src/middleware/auth.js
@@ -1,31 +1,3 @@
-//pichley code ke hain
-
-// const adminAuth=(req, res, next)=> {
-//   const token = "xyz"; // pretend token (usually comes from headers or cookies)
-//   const isAdminAuthorized = token === "xyz";
-
-//   if (isAdminAuthorized) {
-//     next(); // pass control to the next middleware/route handler
-//   } else {
-//     res.status(401).send("Unauthorized request");
-//   }
-// };
-
-// const userAuth=(req, res, next)=> {
-//   const token = "xyz"; // pretend token (usually comes from headers or cookies)
-//   const isUserAuthorized = token === "xyz";
-
-//   if (isUserAuthorized) {
-//     next(); // pass control to the next middleware/route handler
-//   } else {
-//     res.status(401).send("Unauthorized request");
-//   }
-// };
-
-// module.exports={
-//     adminAuth,
-//     userAuth,
-// }
 
 //authentication ke logic 
 
